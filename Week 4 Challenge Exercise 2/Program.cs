463ff5485307376953317cf04075699a8a8132b0
ElijahA1 <elijah.abourezk@outlook.com>
2025-07-29 14:08:05 -0700

    Add project files.

---
+++ Week 4 Challenge Exercise 2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+namespace Week_4_Challenge_Exercise_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(divisibleBy2Or3(15, 30));
+            Console.WriteLine(divisibleBy2Or3(2, 90));
+            Console.WriteLine(divisibleBy2Or3(7, 12));
+        }
+        public static int divisibleBy2Or3(int num1, int num2)
+        {
+            int total;
+            bool condition1 = (num1 % 2 == 0) || (num1 % 3 == 0);
+            bool condition2 = (num2 % 2 == 0) || (num2 % 3 == 0);
+
+            if (condition1 == true && condition2 == true)
+            {
+                total = num1 * num2;
+            } else { total = num1 + num2; }
+
+            return total;
+        }
+
+    }
+}
