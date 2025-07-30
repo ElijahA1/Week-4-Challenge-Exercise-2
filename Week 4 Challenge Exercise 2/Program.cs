0c766161950a5962a370e0cc1d404bd036b5ff12
ElijahA1 <elijah.abourezk@outlook.com>
2025-07-30 14:24:44 -0700

    clean up

--- Week 4 Challenge Exercise 2/Program.cs
+++ Week 4 Challenge Exercise 2/Program.cs
@@ -13,13 +13,15 @@ namespace Week_4_Challenge_Exercise_2
         public static int divisibleBy2Or3(int num1, int num2)
         {
             int total;
+
             bool condition1 = (num1 % 2 == 0) || (num1 % 3 == 0);
             bool condition2 = (num2 % 2 == 0) || (num2 % 3 == 0);
 
-            if (condition1 == true && condition2 == true)
+            if (condition1 && condition2 )
             {
                 total = num1 * num2;
-            } else { total = num1 + num2; }
+            } else 
+            { total = num1 + num2; }
 
             return total;
         }
